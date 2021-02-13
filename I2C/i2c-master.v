4fbefb1252d27ca91eb0152e28fb1ea02e9f1ec6
akhk <akhk@cisco.com>
2021-02-13 18:37:17 +0530

    i2c-tx or master - start stop cycles corrected

--- I2C/i2c-master.v
+++ I2C/i2c-master.v
@@ -22,13 +22,16 @@ module i2c_master(clk, reset, addr, data_wr, data_rd, rw,scl,sda,busy,state,coun
 	output reg busy;					
 	output reg scl, sda;					
 	output reg [3:0]count;									
-	output reg [5:0]state;	
+	output reg [7:0]state;	
 	output reg i2c_clk;	
 	
 	localparam START = 0;					
 	localparam WRITE = 1;					
 	localparam WRITE_DATA = 2;					
 	localparam ACK = 3;					
+	localparam STOP = 4;	
+	localparam STOP2 = 5;
+	localparam LAST = 6;
 	localparam DIVIDE_BY = 4;
 
 	reg counter2 = 0;
@@ -49,7 +52,7 @@ module i2c_master(clk, reset, addr, data_wr, data_rd, rw,scl,sda,busy,state,coun
 		scl <= 1;				
 		sda <= 1;				
 		busy <= 0;				
-		count <= 9;				
+		count <= 8;			
 		state <= START;				
 		end				
 		else				
@@ -57,35 +60,60 @@ module i2c_master(clk, reset, addr, data_wr, data_rd, rw,scl,sda,busy,state,coun
 		START:begin				
 				busy = 1;		
 				sda <= 0; //Start Bit
-				scl <= 1;				
-				count <= 9;		
+				scl <= 1; //SCL Should remain 1				
+				count <= 8;		
 				state = WRITE;		
 				end		
 		WRITE:begin				
 				if (count>0) //>=0 will cause Count to reset to 7		
-				begin		
-				//scl <= i2c_clk;	//Cannot use ~i2c_clk
-				scl <= 1;
-				state <= WRITE_DATA;		
-				end		
-				else		
-				state <= ACK;		
+					begin		
+					scl <= 1;
+					state <= WRITE_DATA;		
+					end
+					
+				else
+					begin
+					scl <= 1;
+					state <= ACK;	
+					end
 				end		
 		WRITE_DATA: begin				
 						if (count == 9)
-							sda <= 0; //Start Bit
+							begin
+							sda <= 0; //Start Bit //This is adding one additional clock cycle
+							count <= count - 1;
+							state <= WRITE;	
+							end
 						else
+							begin
 							sda <= data_wr[count-1];
-						scl <= 0;
-						count <= count - 1;
-						state <= WRITE;
+							scl <= 0; 
+							count <= count - 1;
+							state <= WRITE;	
+							end
+				
 						end
 		ACK:begin				
-			 scl <= 1;						
+			 scl <= 0; 						
+			 state <= STOP;			
+			 end	
+		STOP:begin
+			 scl <= 1; 							
+			 state <= STOP2;	
+			 end
+		STOP2:begin
+			 scl <= 0; 					
 			 sda <= 1; //Stop Bit		
 			 busy <= 0;			
-			 state <= START;			
-			 end			
+			 state <= LAST;
+			 end
+		LAST:begin
+			 scl <= 1;
+			 end
+		default:begin
+				scl<=1;
+				end
+			 
 		endcase				
 	end //end of always					
 						
