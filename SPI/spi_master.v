4b6c7e1ea0fd941288f714772367cb625b621f1a
Akhila K <38599049+Akhila96Rao@users.noreply.github.com>
2021-12-26 10:15:04 +0530

    Corrections for spi_clk

--- SPI/spi_master.v
+++ SPI/spi_master.v
@@ -24,7 +24,9 @@
 //
 // Major Revision
 // User		Date			Description
-// Akhila	6/30/21		Initial Code
+// Akhila	06/30/21		Initial Code
+// Akhila	12/26/21		Corrections for spi_clk behavior
+
 //////////////////////////////////////////////////////////////////////////////////
 module spi_master(clk, spi_clk, reset, cs, miso, mosi, data_wr, state, count, polarity, phase);
 	input clk, reset, polarity, phase;
@@ -57,116 +59,68 @@ module spi_master(clk, spi_clk, reset, cs, miso, mosi, data_wr, state, count, po
 	begin
 	spi_mode = {polarity,phase} ;
 	end
-	
+
+//Inital value for CS and MOSI
+initial begin
+cs = 1;
+mosi = 1;
+spi_clk = 0;
+state = 4'd0;
+miso = 0;
+end
+
 //SPI clk	
-	always@(spi_mode)
-	begin
-	case(spi_mode)
-	2'b00: spi_clk = 0;
-	2'b01: spi_clk = 0;
-	2'b10: spi_clk = 1;
-	2'b11: spi_clk = 1;
-	endcase
-	end
+//	always@(reset, spi_mode)
+//	begin
+////	if (reset == 1'b1)       
+////	begin
+//	case(spi_mode)
+//	2'b00: spi_clk = 0;
+//	2'b01: spi_clk = 0;
+//	2'b10: spi_clk = 1;
+//	2'b11: spi_clk = 1;
+//	endcase
+////	end
+//	end
 
-	always @(posedge clk) 
+	always @(posedge reset, posedge clk) 
 	begin
-	if (counter2 == (DIVIDE_BY/2) - 1) 
+	if (reset == 1)
 	begin
-	spi_clk <= ~spi_clk;
-	counter2 <= 0;
+		case(spi_mode)
+		2'b00: begin
+				spi_clk <= 0;
+				count <= 8;
+				state <= START_A;
+				end
+		2'b01: begin
+				spi_clk <= 0;
+				count <= 8;
+				state <= START_B;
+				end
+		2'b10: begin
+				spi_clk <= 1;
+				count <= 8;
+				state <= START_A;
+				end
+		2'b11: begin
+				spi_clk <= 1;
+				count <= 8;
+				state <= START_B;
+				end
+		endcase
 	end
-	else 
-	counter2 <= counter2 + 1;
+	else
+	begin
+		if (counter2 == (DIVIDE_BY/2) - 1) 
+		begin
+		spi_clk <= ~spi_clk;
+		counter2 <= 0;
+		end
+		else 
+		counter2 <= counter2 + 1;
+	end
 	end 
-
-/*
-//Rising case : Polarity, Phase = 00,10
-	always @ (posedge spi_clk, spi_mode == 2'b00, spi_mode == 2'b10)
-	begin
-	if (reset == 1)
-	begin
-	cs <= 1;
-	count <= 8;
-	mosi <= 1;
-//	if (spi_mode == 2'b00 | spi_mode == 2'b10) begin
-	state <= START_A;
-//	end
-	end
-	
-	else
-	case(state)
-	START_A:begin
-			cs <=0 ; //Can start
-			count <= 8;
-			state <= WRITE_A;
-			end
-	WRITE_A:begin	
-	
-			if (count>0) begin		
-			if (count == 4'd1)begin
-			cs <= 1;
-			end
-			mosi <= data_wr[count-1];
-			count <= count - 1;
-			end
-			
-			else begin
-			state <= ACK_A;	
-			end
-			
-			end
-	ACK_A:begin 
-			cs <= 1;		
-		 end
-	default:begin
-				cs <= 1;
-			  end
-	endcase
-	end
-
-//Falling case : Polarity, Phase = 01,11
-	always @ (negedge spi_clk, spi_mode == 2'b01, spi_mode == 2'b11)
-	begin
-	if (reset == 1)
-	begin
-	cs <= 1;
-	count <= 8;
-	mosi <= 1;
-//	if (spi_mode == 2'b01 | spi_mode == 2'b11) begin
-	state <= START_B;
-//	end
-	end
-	
-	else
-	case(state)
-	START_B:begin
-			cs <=0 ; //Can start
-			count <= 8;
-			state <= WRITE_B;
-			end
-	WRITE_B:begin	
-			if (count>0) begin		
-			if (count == 4'd1) begin
-			cs <= 1;
-			end
-			mosi <= data_wr[count-1];
-			count <= count - 1;
-			end	
-			else begin
-			state <= ACK_B;	
-			end
-			
-			end
-	ACK_B:begin 
-			cs <= 1;		
-		 end
-	default:begin
-				cs <= 1;
-			  end
-	endcase
-	end
-*/
 
 //Rising case : Polarity, Phase = 00,10, //Falling case : Polarity, Phase = 01,11
 	always @ (posedge spi_clk)
@@ -175,10 +129,10 @@ module spi_master(clk, spi_clk, reset, cs, miso, mosi, data_wr, state, count, po
 	begin
 	if (reset == 1)
 	begin
-	cs <= 1;
-	count <= 8;
-	mosi <= 1;
-	state <= START_A;
+//	cs <= 1;
+//	count <= 8;
+//	mosi <= 1;
+//	state <= START_A;
 	end
 	else
 	case(state)
@@ -216,10 +170,10 @@ module spi_master(clk, spi_clk, reset, cs, miso, mosi, data_wr, state, count, po
 	begin
 	if (reset == 1)
 	begin
-	cs <= 1;
-	count <= 8;
-	mosi <= 1;
-	state <= START_B;
+//	cs <= 1;
+//	count <= 8;
+//	mosi <= 1;
+//	state <= START_B;
 	end
 	else
 	case(state)
